975031e7acad37689cefbf5319dcd63acc94849d
Francisco Igor <franciscoigor@gmail.com>
2018-09-26 11:10:17 -0700

    Update docker-machine.sh

--- docker-machine.sh
+++ docker-machine.sh
@@ -18,3 +18,18 @@ docker-machine env
 # Apply environment variables in the current shell session
 eval $(docker-machine env)
 
+# Access SSH console 
+# Syntax: docker-machine ssh {imagename}
+docker-machine ssh default
+
+# Manual way to access SSH 
+# - Get the ip using docker-machine ls
+# Enter using ssh user 'docker' and pass 'tcuser'
+ssh docker@192.168.99.102
+
+# Copy ssh key to enter without password
+ssh-copy-id docker@192.168.99.10
+# Enter password 'tcuser' to copy .ssh/id_rsa.pub to docker machine
+# then you can access without password
+ssh docker@192.168.99.102
+
